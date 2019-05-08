62335cb1b9eefdbea31e5e938fa552f5ad2872a5
Weipeng Zheng <dastonzerg93@gmail.com>
2019-05-07 22:15:12 -0700

    + as tokenizer

--- assign2/src/main/java/WordCount.java
+++ assign2/src/main/java/WordCount.java
@@ -21,7 +21,7 @@ public class WordCount {
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
+            StringTokenizer itr = new StringTokenizer(value.toString(), " +");
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
                 context.write(word, one);
