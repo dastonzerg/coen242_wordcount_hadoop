ce2a2e662c274806aeba1a1bdcb382dbf9383f9a
Weipeng Zheng <dastonzerg93@gmail.com>
2019-05-09 21:11:01 -0700

    StringTokenizer Revised

--- assign2/src/main/java/WordCount.java
+++ assign2/src/main/java/WordCount.java
@@ -31,7 +31,7 @@ public class WordCount {
         @Override
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString(), " +");
+            StringTokenizer itr = new StringTokenizer(value.toString(), "[ +\n]");
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
                 context.write(word, one);
